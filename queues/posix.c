6e1e8ae4486943706c953fd66578b9d3e08c9102
julp <e2c2be7ed0f2f336@gmail.com>
2017-12-18 22:56:00 +0100

    fix includes

--- queues/posix.c
+++ queues/posix.c
@@ -4,10 +4,7 @@
 #include <fcntl.h>
 #include <mqueue.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
